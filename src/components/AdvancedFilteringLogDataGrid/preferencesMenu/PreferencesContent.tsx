f8abac462085149e8ed0422471bd0f5ed73a1ea9
Daniel Allen <dallen@dnsfilter.com>
2023-12-05 08:41:32 -0600

    refactor: preferences code
    Wanted to clean up this code and get it ready for evaluating whether the state of the table is dirty.

--- src/components/AdvancedFilteringLogDataGrid/preferencesMenu/PreferencesContent.tsx
+++ src/components/AdvancedFilteringLogDataGrid/preferencesMenu/PreferencesContent.tsx
@@ -1,112 +1,94 @@
 import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Accordion, { AccordionProps } from "@mui/material/Accordion";
+import AccordionSummary, {
+  AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import { Alert, Button, Grid, Snackbar } from "@mui/material";
-import {
-  ADVANCED_FILTERING_LOG,
-  getDefaultTableState,
-  overrideActiveConfigForSave,
-} from "../initial-state-utils";
-import { useGridApiContext } from "@mui/x-data-grid-pro";
-import { useLocalStorage } from "@/hooks/use-local-storage";
-import { RestartAltOutlined, SaveOutlined } from "@mui/icons-material";
-import PreferencesAccordion from "./PreferencesAccordion";
+import { Grid } from "@mui/material";
+import { ExpandMoreOutlined } from "@mui/icons-material";
+
+const StyledAccordion = styled((props: AccordionProps) => (
+  <Accordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
+
+const StyledAccordionSummary = styled((props: AccordionSummaryProps) => (
+  <AccordionSummary expandIcon={<ExpandMoreOutlined />} {...props} />
+))(({ theme }) => ({
+  backgroundColor: "rgba(0, 0, 0, .03)",
+}));
+
+const StyledAccordionDetails = styled(AccordionDetails)(({ theme }) => ({}));
 
 export default function PreferencesContent() {
-  const apiRef = useGridApiContext();
-  const { update, reset } = useLocalStorage(
-    ADVANCED_FILTERING_LOG,
-    getDefaultTableState()
-  );
+  const [expanded, setExpanded] = React.useState<string | false>();
 
-  const [saveVisible, setSaveVisibile] = React.useState(false);
-  const [defaultVisible, setDefaultVisibile] = React.useState(false);
-
-  const handleSave = () => {
-    update(overrideActiveConfigForSave(apiRef.current.exportState()));
-    setSaveVisibile(true);
-  };
-
-  const handleDefault = () => {
-    reset();
-    apiRef.current.restoreState(getDefaultTableState());
-    setDefaultVisibile(true);
-  };
-
-  const handleSnackbarClose = (
-    event?: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setSaveVisibile(false);
-    setDefaultVisibile(false);
-  };
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
 
   return (
-    <Grid item>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Grid container spacing={2} sx={{ pl: 1, pr: 1, pt: 1 }}>
-            <Grid item xs={12}>
-              <Typography variant="body2">
-                You can override the default table configuration by saving the
-                current configuration as your preference.
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <PreferencesAccordion />
-            </Grid>
-          </Grid>
-        </Grid>
-
-        <Grid item xs={6}>
-          <Button
-            onClick={() => handleSave()}
-            startIcon={<SaveOutlined />}
-            sx={{ mr: 1, pl: 2, pr: 2 }}
+    <Grid container spacing={2} sx={{ pl: 1, pr: 1, pt: 1 }}>
+      <Grid item xs={12}>
+        <Typography variant="body2">
+          You can override the default table configuration by saving the current
+          configuration as your preference.
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <StyledAccordion
+          expanded={expanded === "preferences"}
+          onChange={handleChange("preferences")}
+        >
+          <StyledAccordionSummary
+            aria-controls="panel1d-content"
+            id="preferences"
           >
-            Save
-          </Button>
-          <Snackbar
-            open={saveVisible}
-            autoHideDuration={3000}
-            onClose={handleSnackbarClose}
-            anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          >
-            <Alert
-              onClose={handleSnackbarClose}
-              severity="success"
-              color="info"
-            >
-              Table Preferences Saved!
-            </Alert>
-          </Snackbar>
-        </Grid>
-        <Grid item xs={6} textAlign="end">
-          <Button
-            onClick={() => handleDefault()}
-            startIcon={<RestartAltOutlined />}
-            sx={{ pl: 2, pr: 2 }}
-          >
-            Use Default
-          </Button>
-          <Snackbar
-            open={defaultVisible}
-            autoHideDuration={3000}
-            onClose={handleSnackbarClose}
-            anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          >
-            <Alert
-              onClose={handleSnackbarClose}
-              severity="success"
-              color="info"
-            >
-              Default configuration restored!
-            </Alert>
-          </Snackbar>
-        </Grid>
+            <Typography variant="body2">
+              Preference that will be saved
+            </Typography>
+          </StyledAccordionSummary>
+          <StyledAccordionDetails>
+            <ul>
+              <li>
+                <Typography variant="body2">Active Filters</Typography>
+              </li>
+              <li>
+                <Typography variant="body2">Columns</Typography>
+                <ul>
+                  <li>
+                    <Typography variant="body2">Visibility</Typography>
+                  </li>
+                  <li>
+                    <Typography variant="body2">Order</Typography>
+                  </li>
+                  <li>
+                    <Typography variant="body2">Width</Typography>
+                  </li>
+                  <li>
+                    <Typography variant="body2">Sort</Typography>
+                  </li>
+                  <li>
+                    <Typography variant="body2">Pinned</Typography>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Typography variant="body2">Page Size</Typography>
+              </li>
+            </ul>
+          </StyledAccordionDetails>
+        </StyledAccordion>
       </Grid>
     </Grid>
   );
--- src/components/AdvancedFilteringLogDataGrid/preferencesMenu/PreferencesMenu.tsx
+++ src/components/AdvancedFilteringLogDataGrid/preferencesMenu/PreferencesMenu.tsx
@@ -1,50 +1,139 @@
 import * as React from "react";
 import Popover from "@mui/material/Popover";
-import Button from "@mui/material/Button";
 import { TuneOutlined } from "@mui/icons-material";
 import PreferencesContent from "./PreferencesContent";
-import { Grid } from "@mui/material";
+import { Alert, Button, Grid, Snackbar } from "@mui/material";
+import {
+  ADVANCED_FILTERING_LOG,
+  getDefaultTableState,
+  overrideActiveConfigForSave,
+} from "../initial-state-utils";
+import { useGridApiContext } from "@mui/x-data-grid-pro";
+import { useLocalStorage } from "@/hooks/use-local-storage";
+import { RestartAltOutlined, SaveOutlined } from "@mui/icons-material";
 
 export default function PreferencesPopover() {
+  // state and hooks
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
     null
   );
+  const [saveVisible, setSaveVisibile] = React.useState(false);
+  const [defaultVisible, setDefaultVisibile] = React.useState(false);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const apiRef = useGridApiContext();
+  const { update, reset } = useLocalStorage(
+    ADVANCED_FILTERING_LOG,
+    getDefaultTableState()
+  );
+
+  // event handlers
+  const handleSavePreferences = () => {
+    update(overrideActiveConfigForSave(apiRef.current.exportState()));
+    setSaveVisibile(true);
+    handleClosePreferences();
+  };
+
+  const handleDefaultPreferences = () => {
+    reset();
+    apiRef.current.restoreState(getDefaultTableState());
+    setDefaultVisibile(true);
+    handleClosePreferences();
+  };
+
+  const handleSnackbarClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSaveVisibile(false);
+    setDefaultVisibile(false);
+  };
+
+  const handleOpenPreferences = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleClosePreferences = () => {
     setAnchorEl(null);
   };
 
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
+  const POPOVER_ID = "preferences-popover";
 
   return (
     <>
       <Button
-        aria-describedby={id}
-        onClick={handleClick}
+        aria-describedby={POPOVER_ID}
+        onClick={handleOpenPreferences}
         startIcon={<TuneOutlined />}
         sx={{ mr: 1, pl: 2, pr: 2 }}
       >
         Preferences
       </Button>
       <Popover
-        id={id}
-        open={open}
+        id={POPOVER_ID}
+        open={Boolean(anchorEl)}
         anchorEl={anchorEl}
-        onClose={handleClose}
+        onClose={handleClosePreferences}
         anchorOrigin={{
           vertical: "bottom",
           horizontal: "left",
         }}
       >
-        <Grid container alignItems="center" sx={{ p: 0.5, maxWidth: 400 }}>
-          <PreferencesContent />
+        <Grid
+          container
+          alignItems="center"
+          sx={{ p: 0.5, maxWidth: 400 }}
+          spacing={2}
+        >
+          <Grid item xs={12}>
+            <PreferencesContent />
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button
+              onClick={() => handleSavePreferences()}
+              startIcon={<SaveOutlined />}
+              sx={{ mr: 1, pl: 2, pr: 2 }}
+            >
+              Save
+            </Button>
+          </Grid>
+          <Grid item xs={6} textAlign="end">
+            <Button
+              onClick={() => handleDefaultPreferences()}
+              startIcon={<RestartAltOutlined />}
+              sx={{ pl: 2, pr: 2 }}
+            >
+              Use Default
+            </Button>
+          </Grid>
         </Grid>
       </Popover>
+      <Snackbar
+        open={saveVisible}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert onClose={handleSnackbarClose} severity="success" color="info">
+          Table Preferences Saved!
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={defaultVisible}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert onClose={handleSnackbarClose} severity="success" color="info">
+          Default configuration restored!
+        </Alert>
+      </Snackbar>
     </>
   );
 }
